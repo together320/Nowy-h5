158bb898506ec8138130b7c7012513f8ed3d5832
wengxiaohu <wengxiaohu@gjzq.com.cn>
2023-05-02 15:20:51 +0800

    fix google map bug

--- plugins/vueGoogleMaps.js
+++ plugins/vueGoogleMaps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyAlJPnrzSZcZOwmeUdk44Hle7_4FoE8-tw',
+    key: 'Atw',
     libraries:  ['places', 'geometry'], // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
